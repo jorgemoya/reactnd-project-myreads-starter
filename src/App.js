618cd580f9ab7538fdabec58e1e7890804c3cec8
Jorge Moya <jorge.moya@bigcommerce.com>
2019-03-27 11:40:09 -0500

    refactor: use react-router to handle routes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import React from "react";
 import "./App.css";
 import Search from "./components/Search";
 import BooksList from "./components/BooksList";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  };
-
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? <Search /> : <BooksList />}
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Switch>
+            <Route exact path="/" component={BooksList} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
